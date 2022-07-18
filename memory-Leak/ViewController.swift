f39fb53ff091ffea315810ac952f8e1fb59c588a
Waleed Ahmed <waleed.wit@gmail.com>
2022-07-18 11:06:04 +0300

    find memory leak

--- memory-Leak/ViewController.swift
+++ memory-Leak/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+    var animal: Parent?
+    var cat: Child?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+ 
+        cat = Child()
+        animal = Parent()
+
+        cat?.parent = animal
+        animal?.child = cat
+
+        cat = nil
+        animal = nil
+   
+        
+        //MARK: output
+        
+        //Parent denit called
+        //Child deinit called
     }
 
-
+    
 }
 
+
+class Child {
+    var parent: Parent?
+
+    deinit {
+        print("Child deinit called")
+    }
+}
+
+class Parent {
+    var child: Child?
+
+    deinit {
+        print("Parent denit called")
+    }
+}
